73f87a046f2e440f7172eb92146fcefe728589ec
Przemysław Matkowski <matkowski1994@gmail.com>
2019-12-13 14:01:41 +0100

    update

--- formularz.php
+++ formularz.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl-PL">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <link rel="stylesheet" href="style/style.css" />
     <title>Formularz Smartbees</title>
   </head>
@@ -48,11 +47,9 @@
           <label for="message">Wiadomość</label>
           <textarea name="message" id="message" cols="30" rows="5"></textarea>
         </div>
- 
         <button type="submit" id="contactForm__btn" name="submit">
           Wyślij
         </button>
-
       </form>
       </div>
       <div class="container">
